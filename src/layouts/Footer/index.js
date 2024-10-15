fe796d4d95051cb197a7be68767ef12374524d1b
diegojoseup <diego.joseupwork@gmail.com>
2024-10-15 02:44:38 -0700

    layout optimize

--- src/layouts/Footer/index.js
+++ src/layouts/Footer/index.js
@@ -1,36 +1,43 @@
-import {
-    Box, 
-    Stack,
-    Typography,
+import { Box, Stack, Typography } from "@mui/material";
 
-} from '@mui/material';
-
-export default function Footer () {
-    return (
-        <Stack 
-            alignItems="center"
-            sx={{ py: 7 }}
+export default function Footer() {
+  return (
+    <Stack alignItems="center" sx={{ py: 7 }}>
+      <Stack
+        alignItems="center"
+        gap={5}
+        sx={{
+          maxWidth: 560,
+        }}
+      >
+        <Box component="img" src="/images/logo-white.png" />
+        <Typography
+          sx={{
+            textTransform: "uppercase",
+            color: "#9DAFBD",
+            fontFamily: "Roboto",
+          }}
         >
-            <Stack
-                alignItems="center"
-                gap={5}
-                sx={{
-                    maxWidth: 560
-                }}
-            >
-                <Box component="img" src="/images/logo-white.png" />
-                <Typography sx={{
-                    textTransform: 'uppercase',
-                    color: '#9DAFBD',
-                    fontFamily: 'Roboto'
-                }}>About Us</Typography>
-                <Typography variant="h4">H3M</Typography>
-                <Typography sx={{ textAlign: 'center', px: 4 }}>We are an immersive media company providing services and tools to companies that want a 360 virtual presence in the Metaverse.</Typography>
-                <Stack alignItems="center">
-                    <Typography sx={{ color: '#9DAFBD', fontWeight: 300, fontFamily: 'Roboto' }}>Copyright © H3M Inc</Typography>
-                    <Typography sx={{ color: '#9DAFBD', fontWeight: 300, fontFamily: 'Roboto' }}>All rights reserved</Typography>
-                </Stack>
-            </Stack>
+          About Us
+        </Typography>
+        <Typography variant="h4">H3M</Typography>
+        <Typography sx={{ textAlign: "center", px: 4 }}>
+          We are an immersive media company providing services and tools to
+          companies that want a 360 virtual presence in the Metaverse.
+        </Typography>
+        <Stack alignItems="center">
+          <Typography
+            sx={{ color: "#9DAFBD", fontWeight: 300, fontFamily: "Roboto" }}
+          >
+            Copyright © H3M Inc
+          </Typography>
+          <Typography
+            sx={{ color: "#9DAFBD", fontWeight: 300, fontFamily: "Roboto" }}
+          >
+            All rights reserved
+          </Typography>
         </Stack>
-    );
+      </Stack>
+    </Stack>
+  );
 }
